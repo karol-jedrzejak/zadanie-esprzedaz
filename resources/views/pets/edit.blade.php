8f75781d904e258f3619059c92a499147f9d176f
karol-jedrzejak <karol.jedrzejak@gmail.com>
2025-01-16 14:50:55 +0100

    up

--- resources/views/pets/edit.blade.php
+++ resources/views/pets/edit.blade.php
@@ -10,6 +10,49 @@
             <input name="_method" type="hidden" value="PUT">
             <input name="id" type="hidden" value="{{$pet->id}}">
             <!-- X-XSRF-TOKEN -->
+
+            {{--
+            https://laravel.io/forum/04-23-2015-validation-form-with-dynamic-inputs
+
+            <div class="mb-3 col-12">
+                <label for="company" class="form-label">Firma</label>
+                <select class="form-select" id="company" name="company" onchange="updateEmployeesList();" required>
+                        <option value=''>--- Wybierz Firmę ---</option>
+                    @foreach ($companies as $company)
+                        <option
+                        @if(old('company') == $company->id) selected='selected' @endif
+                        value={{$company->id}}>{{$company->name_short}} [{{$company->name_complete}}]</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="mb-3 col-10" id="employee_col_0">
+                <label for="employee_0" id="employee_label_0" class="form-label">Pracownik 1</label>
+                <select class="form-select" id="employee_0" name="employee_[0]" required>
+                    <option value=''>--- Wybierz Pracownika ---</option>
+                    @foreach ($employees as $employee)
+                        <option
+                        @if(old('employee_[0]') == $employee->id) selected='selected' @endif
+                        value={{$employee->id}} data-company={{$employee->company}}>{{$employee->name}} {{$employee->surname}}</option>
+                    @endforeach
+                </select>
+            </div>
+
+
+
+            <div class="mb-3 col-2">
+                <label for="employee_add" class="form-label">&nbsp;</label>
+                <button id="employee_add" type="button" class="btn btn-primary w-100" onclick="addEmployee();">Dodaj</button>
+            </div>
+
+            <div class="mb-3 col-2" id="employess_col_del_0" hidden>
+                <label for="employee_del_0" id="employee_del_label_0" class="form-label">&nbsp;</label>
+                <button id="employee_del_0" type="button" class="btn btn-danger w-100" onclick="">Usuń</button>
+            </div>
+            --}}
+
+
+
             <div class="form-group mb-2">
                 <label for="cat_id" class="mb-2">Category ID</label>
                 <input type="number" class="form-control" name="cat_id" id="cat_id" value="{{$pet->category->id}}">
